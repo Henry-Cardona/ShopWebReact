b7a27a915c8b57bd831fdf6a7dafb0ac3e9a85a7
Henry-Cardona <henrycardona99@gmail.com>
2023-03-28 18:38:57 -0600

    Cambio de nombre a un archivo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import store from './store';
 import { Provider } from 'react-redux';
-import GridProducts from './components/GridProducts.js';
+import GridProducts from './components/gridProducts.js';
 
 function App() {
   return (
